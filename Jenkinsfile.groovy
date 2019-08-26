6d5470ad3bff1e629b5c4b88b3fc685c3a0b8e8a
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-25 21:41:27 -0500

    create

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,6 +6,20 @@ node('master') {
     stage('Clone repo') {
     git 'https://github.com/dilfuza97/Kubernetes_jenkins.git'
   }
+    stage("Download Terraform"){
+          steps{
+              ws("tmp/"){
+                  script {
+                      def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                      if (exists) {
+                          sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                          sh "sudo mv terraform /bin"
+                          sh "terraform version"
+                      } else {
+                          sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                          sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                          sh "sudo mv terraform /bin"
+                          sh "terraform version"
 
    stage("Terraform init") {
      dir("${WORKSPACE}/Kubernetes_jenkins/artemis.tf") {
@@ -30,6 +44,10 @@ node('master') {
            }
          }
        }
-
-
+     }
+}
+    }
+}
+       }
+    }
 }
